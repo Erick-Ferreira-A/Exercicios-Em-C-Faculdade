e53d4221a26278337c12c73a23da80e66887bc3c
Erick Ferreira <erickfaleixo@gmail.com>
2025-08-22 09:05:05 -0300

    Create ex001_while.c

---
+++ While/ex001_while.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+	// Agosto de 2025
+	// Professor Richard
+	// Aluno: Erick Ferreira Aleixo 
+	// RA 231430
+	// UNASP Campus SP
+
+int main() {
+	
+	int i = 1;
+	
+	printf("------------- CONTAGEM CRESCENTE------------\n");
+	
+	while (i <= 10) {
+		printf("valor de i: %i\n", i);
+		
+		i++;
+	}
+	
+	return 0;
+	
+}
